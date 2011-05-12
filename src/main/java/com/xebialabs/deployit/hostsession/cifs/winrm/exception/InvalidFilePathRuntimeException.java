7bcbf4a846993db3cb9556396cca32188e4c7ef0
Benoit Moussaud <bmoussaud@xebialabs.com>
2011-05-12 06:52:07 +0200

    GPL License

--- src/main/java/com/xebialabs/deployit/hostsession/cifs/winrm/exception/InvalidFilePathRuntimeException.java
+++ src/main/java/com/xebialabs/deployit/hostsession/cifs/winrm/exception/InvalidFilePathRuntimeException.java
@@ -1,18 +1,18 @@
 /*
- * This file is part of Maven Deployit plugin.
+ * This file is part of WinRM.
  *
- * WinRM Java implementation is free software: you can redistribute it and/or modify
+ * WinRM is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * WinRM Java implementation is distributed in the hope that it will be useful,
+ * WinRM is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Maven Deployit plugin.  If not, see <http://www.gnu.org/licenses/>.
+ * along with WinRM.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.xebialabs.deployit.hostsession.cifs.winrm.exception;
 
